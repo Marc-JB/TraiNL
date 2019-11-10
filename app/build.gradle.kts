c191c9cd4468b6d96bd7fcb81118736dd91c54f3
Marc <16156117+Marc-JB@users.noreply.github.com>
2019-11-10 11:40:56 +0100

    Bugfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,8 @@ android {
         }
     }
 
+    dataBinding.isEnabled = true
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
