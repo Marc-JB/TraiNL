555b478a806691bef93745e943ca0b60eb4172b1
Marc <16156117+Marc-JB@users.noreply.github.com>
2019-11-10 15:00:43 +0100

    Add longer timeout

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -12,7 +12,10 @@ dependencies {
 
     // Other
     implementation(kotlin("stdlib-jdk8", "1.3.50"))
+
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.1")
+
+    implementation("com.squareup.okhttp3:okhttp:4.2.1")
     implementation("com.squareup.retrofit2:retrofit:2.6.2")
     implementation("com.squareup.retrofit2:converter-gson:2.6.2")
 }
--- api/src/main/java/nl/marc_apps/ovgo/api/OVgoApiRepository.kt
+++ api/src/main/java/nl/marc_apps/ovgo/api/OVgoApiRepository.kt
@@ -4,8 +4,10 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.withContext
 import nl.marc_apps.ovgo.domain.services.PublicTransportDataRepository
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class OVgoApiRepository(override var language: String = "en") : PublicTransportDataRepository {
     override suspend fun getDepartures(station: String) = coroutineScope {
@@ -21,9 +23,17 @@ class OVgoApiRepository(override var language: String = "en") : PublicTransportD
     }
 
     companion object {
+        private val httpClient: OkHttpClient = OkHttpClient.Builder()
+            .callTimeout(1, TimeUnit.MINUTES)
+            .connectTimeout(45, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+
         private val api: OVgoApiSpecification = Retrofit.Builder()
             .baseUrl("https://ovgo.herokuapp.com/api/v1/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(httpClient)
             .build()
             .create<OVgoApiSpecification>(OVgoApiSpecification::class.java)
     }
