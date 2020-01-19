b20553bd298a7fdf762425af48f5705f55b4b3f2
Marc <16156117+Marc-JB@users.noreply.github.com>
2020-01-19 18:57:54 +0100

    Update versions

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -56,12 +56,12 @@ dependencies {
     implementation(project(":domain"))
 
     // General libraries
-    implementation(kotlin("stdlib-jdk8", "1.3.50"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.61")
 
     implementation("androidx.appcompat:appcompat:1.1.0")
     implementation("androidx.core:core-ktx:1.1.0")
 
-    implementation("com.google.android.material:material:1.1.0-beta02")
+    implementation("com.google.android.material:material:1.1.0-rc02")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
 
@@ -72,8 +72,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.1.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.1.0")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.2.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.3")
 
     implementation("com.squareup.picasso:picasso:2.71828")
 
@@ -82,7 +82,7 @@ dependencies {
     implementation("org.koin:koin-android-viewmodel:2.0.1")
 
     // Testing libraries
-    testImplementation("junit:junit:4.12")
+    testImplementation("junit:junit:4.13")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
