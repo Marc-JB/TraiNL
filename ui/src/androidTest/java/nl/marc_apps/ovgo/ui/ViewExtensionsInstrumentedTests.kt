a7d425390b2688022c2755c0501d43dbafe4bcd8
Marc <16156117+Marc-JB@users.noreply.github.com>
2020-02-01 17:14:12 +0100

    Add some tests

---
+++ ui/src/androidTest/java/nl/marc_apps/ovgo/ui/ViewExtensionsInstrumentedTests.kt
@@ -0,0 +1,26 @@
+package nl.marc_apps.ovgo.ui
+
+import android.widget.ImageView
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ViewExtensionsInstrumentedTests {
+    @Test
+    fun imageListLoadImagesRemovesAllViewsInLayoutFirst() {
+        // Arrange
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val layout = ImageList(appContext)
+        val child = ImageView(appContext)
+        layout.addView(child)
+
+        // Act
+        loadImages(layout, emptyList())
+
+        // Assert
+        assertEquals(layout.childCount, 0)
+    }
+}
