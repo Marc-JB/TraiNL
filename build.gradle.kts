e7ea7656f579ee179ab6fa7f547ef5e64b9abc85
Marc <16156117+Marc-JB@users.noreply.github.com>
2020-07-05 21:46:14 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.0.0")
-        classpath(kotlin("gradle-plugin", "1.3.61"))
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.2.0")
+        classpath(kotlin("gradle-plugin", "1.3.72"))
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.0")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
