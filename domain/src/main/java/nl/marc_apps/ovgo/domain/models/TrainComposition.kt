2ab70b62f67727cb114ada4673fada2214cd7e66
Marc <16156117+Marc-JB@users.noreply.github.com>
2020-10-24 16:26:23 +0200

    Fix crash

--- domain/src/main/java/nl/marc_apps/ovgo/domain/models/TrainComposition.kt
+++ domain/src/main/java/nl/marc_apps/ovgo/domain/models/TrainComposition.kt
@@ -1,16 +1,16 @@
 package nl.marc_apps.ovgo.domain.models
 
 data class TrainComposition(
-    val id: Short,
+    val id: Short? = null,
     val station: TrainStation?,
-    val type: String,
-    val operator: String,
-    val platform: String,
+    val type: String? = null,
+    val operator: String? = null,
+    val platform: String? = null,
     val parts: Array<TrainPart>,
     val shortened: Boolean,
-    val actualNumberOfCoaches: Byte,
-    val plannedNumberOfCoaches: Byte,
-    val length: Short,
+    val actualNumberOfCoaches: Byte? = null,
+    val plannedNumberOfCoaches: Byte? = null,
+    val length: Short?,
     //val crowdsForecast: Array<Object>,
     val facilities: TrainFacilities,
     val seats: Short,
