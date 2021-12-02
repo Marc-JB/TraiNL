43560c3a010481a20d21cfd9f6b98f4fb50fa4f9
Marc <16156117+Marc-JB@users.noreply.github.com>
2021-12-02 13:42:17 +0100

    Fix parsing of JSON timestamps with an explicit timezone

--- app/src/main/java/nl/marc_apps/ovgo/utils/serialization/JsonDateTime.kt
+++ app/src/main/java/nl/marc_apps/ovgo/utils/serialization/JsonDateTime.kt
@@ -6,11 +6,11 @@ import java.util.*
 object JsonDateTime {
     private const val JSON_DATE_AND_TIME_PATTERN_UTC = "yyyy-MM-dd'T'HH:mm:ss'Z'"
 
-    private const val JSON_DATE_AND_TIME_PATTERN_LOCAL = "yyyy-MM-dd'T'HH:mm:ssZ"
+    private const val JSON_DATE_AND_TIME_PATTERN_LOCAL = "yyyy-MM-dd'T'HH:mm:ssX"
 
     private const val JSON_DATE_AND_TIME_PATTERN_UTC_PRECISE = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
 
-    private const val JSON_DATE_AND_TIME_PATTERN_LOCAL_PRECISE = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    private const val JSON_DATE_AND_TIME_PATTERN_LOCAL_PRECISE = "yyyy-MM-dd'T'HH:mm:ss.SSSX"
 
     val defaultParser = SimpleDateFormat(JSON_DATE_AND_TIME_PATTERN_UTC_PRECISE, Locale.US)
 
