4b9b036606a1dec26b065286929cc6f91d5f9782
Marc <16156117+Marc-JB@users.noreply.github.com>
2021-12-03 14:45:06 +0100

    Add Jaro and JaroWinkler search algorithm tests

---
+++ app/src/test/java/nl/marc_apps/ovgo/search/JaroWinklerStringSimilarityTests.kt
@@ -0,0 +1,83 @@
+package nl.marc_apps.ovgo.search
+
+import kotlin.test.*
+
+class JaroWinklerStringSimilarityTests {
+    @Test
+    fun similarityIsZeroWhenFirstStringIsEmpty() {
+        // Arrange
+        val firstString = ""
+        val secondString = "ABC"
+
+        // Act
+        val result = JaroWinklerStringSimilarity.calculateSimilarity(firstString, secondString)
+
+        // Assert
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun similarityIsZeroWhenSecondStringIsEmpty() {
+        // Arrange
+        val firstString = "ABC"
+        val secondString = ""
+
+        // Act
+        val result = JaroWinklerStringSimilarity.calculateSimilarity(firstString, secondString)
+
+        // Assert
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun similarityIsOneThousandWhenBothStringsAreEqual() {
+        // Arrange
+        val firstString = "ABCDEF"
+        val secondString = "ABCDEF"
+
+        // Act
+        val result = JaroWinklerStringSimilarity.calculateSimilarity(firstString, secondString)
+
+        // Assert
+        assertEquals(1000, result)
+    }
+
+    @Test
+    fun similarityIsOneThousandWhenBothStringsAreEmpty() {
+        // Arrange
+        val firstString = ""
+        val secondString = ""
+
+        // Act
+        val result = JaroWinklerStringSimilarity.calculateSimilarity(firstString, secondString)
+
+        // Assert
+        assertEquals(1000, result)
+    }
+
+    @Test
+    fun testWordSimilarity1() {
+        // Arrange
+        val firstString = "TRATE"
+        val secondString = "TRACE"
+
+        // Act
+        val result = JaroWinklerStringSimilarity.calculateSimilarity(firstString, secondString)
+
+        // Assert
+        assertEquals(906, result)
+    }
+
+    @Test
+    fun testWordSimilarity2() {
+        // Arrange
+        val firstString = "DwAyNE"
+        val secondString = "DuANE"
+
+        // Act
+        val result = JaroWinklerStringSimilarity.calculateSimilarity(firstString, secondString)
+
+        // Assert
+        assertEquals(839, result)
+    }
+}
