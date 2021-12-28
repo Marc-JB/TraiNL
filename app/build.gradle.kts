50c217716ed9e0f027c720fa65a4ecd091eb14a2
Marc <16156117+Marc-JB@users.noreply.github.com>
2021-12-28 21:31:08 +0100

    Update version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,8 @@ android {
         applicationId = "nl.marc_apps.ovgo"
         minSdk = 21
         targetSdk = 31
-        versionCode = getProperty("version.code")?.toInt() ?: 9
-        versionName = getProperty("version.name") ?: "0.6.1"
+        versionCode = getProperty("version.code")?.toInt() ?: 10
+        versionName = getProperty("version.name") ?: "0.7"
 
         testBuildType = "debug"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
