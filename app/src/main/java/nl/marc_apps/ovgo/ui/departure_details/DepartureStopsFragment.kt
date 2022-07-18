e83c4da58df3d191207287245504f6614512c959
Marc <16156117+Marc-JB@users.noreply.github.com>
2022-07-18 16:54:02 +0200

    Migrate journey stops to Compose

--- app/src/main/java/nl/marc_apps/ovgo/ui/departure_details/DepartureStopsFragment.kt
+++ app/src/main/java/nl/marc_apps/ovgo/ui/departure_details/DepartureStopsFragment.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.content.res.AppCompatResources
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import nl.marc_apps.ovgo.R
 import nl.marc_apps.ovgo.databinding.FragmentDepartureStopsBinding
-import nl.marc_apps.ovgo.ui.DividerItemDecoration
+import nl.marc_apps.ovgo.ui.theme.AppTheme
 
 class DepartureStopsFragment : BottomSheetDialogFragment() {
     private lateinit var binding: FragmentDepartureStopsBinding
@@ -31,22 +33,18 @@ class DepartureStopsFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val departureStopsAdapter = DepartureStopsAdapter {
-            val action = DepartureStopsFragmentDirections
-                .actionDepartureStopsToStationDepartureBoard(it)
-            activity?.findNavController(R.id.holder_fragment)?.navigate(action)
-        }
-        binding.listStops.adapter = departureStopsAdapter
-        binding.listStops.addItemDecoration(
-            DividerItemDecoration(
-                view.context,
-                DividerItemDecoration.VERTICAL
-            ).apply {
-                AppCompatResources.getDrawable(view.context, R.drawable.divider)?.let {
-                    drawable = it
+        binding.root.setViewCompositionStrategy(
+            ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
+        )
+
+        binding.root.setContent {
+            AppTheme {
+                Surface(
+                    color = MaterialTheme.colors.background
+                ) {
+                    DepartureStopsView(navigationArgs.stops, activity?.findNavController(R.id.holder_fragment))
                 }
             }
-        )
-        departureStopsAdapter.submitList(navigationArgs.stops.toList())
+        }
     }
 }
---
+++ app/src/main/java/nl/marc_apps/ovgo/ui/departure_details/DepartureStopsView.kt
@@ -0,0 +1,200 @@
+package nl.marc_apps.ovgo.ui.departure_details
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalFontLoader
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.ParagraphIntrinsics
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import kotlinx.datetime.Instant
+import kotlinx.datetime.toJavaInstant
+import nl.marc_apps.ovgo.R
+import nl.marc_apps.ovgo.domain.JourneyStop
+import nl.marc_apps.ovgo.domain.TrainStation
+import nl.marc_apps.ovgo.ui.theme.AppTheme
+import nl.marc_apps.ovgo.utils.format
+import java.text.DateFormat
+import kotlin.time.Duration
+
+@Composable
+fun DepartureStopsView(stops: Array<JourneyStop>, navController: NavController?) {
+    DepartureStopsView(stops.toList()) {
+        val action = DepartureStopsFragmentDirections
+            .actionDepartureStopsToStationDepartureBoard(it)
+        navController?.navigate(action)
+    }
+}
+
+private fun isForeignStation(trainStation: TrainStation): Boolean {
+    return trainStation.country != null && trainStation.country != TrainStation.Country.THE_NETHERLANDS
+}
+
+@Composable
+fun DepartureStopsView(stops: List<JourneyStop>, onStationSelected: (TrainStation) -> Unit) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(Modifier.height(16.dp))
+
+        Pill()
+
+        Spacer(Modifier.height(16.dp))
+
+        LazyColumn {
+            items(stops, key = { it.id }) {
+                val intrinsics = ParagraphIntrinsics(
+                    "00:00 +00",
+                    style = MaterialTheme.typography.subtitle1,
+                    density = LocalDensity.current,
+                    resourceLoader = LocalFontLoader.current
+                )
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .clickable {
+                            onStationSelected(it.station)
+                        }
+                        .heightIn(min = 56.dp)
+                        .padding(16.dp, 8.dp)
+                        .fillMaxWidth()
+                ) {
+                    Column(
+                        Modifier.widthIn(min = with(LocalDensity.current) {
+                            intrinsics.maxIntrinsicWidth.toDp()
+                        })
+                    ) {
+                        val hasSingleTime = it.actualArrivalTime == it.actualDepartureTime && !it.isDepartureDelayed && !it.isArrivalDelayed
+                        if (!hasSingleTime && it.plannedArrivalTime != null) {
+                            TimeDisplayView(it.plannedArrivalTime, it.arrivalDelay)
+                            Spacer(Modifier.width(2.dp))
+                        }
+
+                        if (it.plannedDepartureTime != null) {
+                            Spacer(Modifier.width(2.dp))
+                            TimeDisplayView(it.plannedDepartureTime, it.departureDelay)
+                        }
+                    }
+
+                    Spacer(Modifier.width(8.dp))
+
+                    Text(
+                        buildString {
+                            append(it.station.fullName)
+
+                            if (isForeignStation(it.station)) {
+                                append(" ")
+                                append(it.station.country?.flag)
+                            }
+                        },
+                        color = colorResource(if (it.isCancelled) R.color.sectionTitleWarningColor else R.color.sectionTitleColor),
+                        style = MaterialTheme.typography.subtitle1
+                    )
+                }
+
+                if (it != stops.last()) {
+                    Divider(
+                        modifier = Modifier.padding(16.dp, 0.dp)
+                    )
+                }
+            }
+
+            val spaceToInsert = maxOf(5 - stops.size, 0)
+
+            items(spaceToInsert, key = { "space-$it" }) {
+                Spacer(Modifier.height(56.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun TimeDisplayView(plannedTime: Instant, delay: Duration) {
+    val delayInMinutesRounded = delay.inWholeMinutes
+    val isDelayed = delayInMinutesRounded > 0
+
+    val departureTimeWithDelayText = if (isDelayed) {
+        stringResource(
+            R.string.departure_time_delayed,
+            plannedTime.format(timeStyle = DateFormat.SHORT),
+            delayInMinutesRounded
+        )
+    } else {
+        plannedTime.format(timeStyle = DateFormat.SHORT)
+    }
+
+    Text(
+        departureTimeWithDelayText,
+        color = colorResource(if(isDelayed) R.color.sectionTitleWarningColor else R.color.sectionTitleOkColor),
+        style = MaterialTheme.typography.subtitle1
+    )
+}
+
+@Composable
+fun Pill() {
+    Box(
+        modifier = Modifier
+            .size(32.dp, 8.dp)
+            .clip(shape = RoundedCornerShape(4.dp))
+            .background(colorResource(R.color.bottomSheetPillColor))
+    ) {}
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun DepartureStopsViewPreview() {
+    val stops = listOf(
+        JourneyStop(
+            "abcd",
+            TrainStation("ddr", "Dordrecht", "Dordrecht"),
+            null,
+            null,
+            Instant.parse("2022-07-18T10:20Z").toJavaInstant(),
+            Instant.parse("2022-07-18T10:20Z").toJavaInstant(),
+            "3a",
+            "3a"
+        ),
+        JourneyStop(
+            "efgh",
+            TrainStation("rtd", "Rotterdam Centraal", "Rotterdam", setOf("Rotterdam CS", "Rotterdam")),
+            Instant.parse("2022-07-18T10:40Z").toJavaInstant(),
+            Instant.parse("2022-07-18T10:40Z").toJavaInstant(),
+            Instant.parse("2022-07-18T10:43Z").toJavaInstant(),
+            Instant.parse("2022-07-18T10:44Z").toJavaInstant(),
+            "6",
+            "7"
+        ),
+        JourneyStop(
+            "ijkl",
+            TrainStation("gvc", "Den Haag Centraal", "Den Haag", setOf("Den Haag CS", "Den Haag")),
+            Instant.parse("2022-07-18T10:40Z").toJavaInstant(),
+            Instant.parse("2022-07-18T10:40Z").toJavaInstant(),
+            null,
+            null,
+            "20",
+            "20"
+        )
+    )
+    AppTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            DepartureStopsView(stops) {}
+        }
+    }
+}
