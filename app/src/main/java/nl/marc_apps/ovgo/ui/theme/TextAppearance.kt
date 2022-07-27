cd4209b8855446f4cd3bb7d3ec54adcca922e0de
Marc <16156117+Marc-JB@users.noreply.github.com>
2022-07-27 10:56:10 +0200

    Use supplied Roboto fonts

--- app/src/main/java/nl/marc_apps/ovgo/ui/theme/TextAppearance.kt
+++ app/src/main/java/nl/marc_apps/ovgo/ui/theme/TextAppearance.kt
@@ -1,13 +1,21 @@
 package nl.marc_apps.ovgo.ui.theme
 
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import nl.marc_apps.ovgo.R
 
 val TextAppearance
     @Composable
-    get() = MaterialTheme.typography.copy(
+    get() = Typography(
+        defaultFontFamily = FontFamily(
+            Font(R.font.roboto_regular, FontWeight.Normal),
+            Font(R.font.roboto_bold, FontWeight.Bold),
+        ),
         subtitle1 = MaterialTheme.typography.subtitle1.copy(
             fontWeight = FontWeight.Bold
         ),
