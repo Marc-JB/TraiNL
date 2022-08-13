6bf98d1655bafaf3fcc4f1bdd14af398df5ebdb4
Marc <16156117+Marc-JB@users.noreply.github.com>
2022-08-13 18:05:52 +0200

    Remove grid for large screen devices

--- app/src/main/java/nl/marc_apps/ovgo/ui/departure_board/DeparturesList.kt
+++ app/src/main/java/nl/marc_apps/ovgo/ui/departure_board/DeparturesList.kt
@@ -3,20 +3,15 @@ package nl.marc_apps.ovgo.ui.departure_board
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.booleanResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.ImageLoader
-import nl.marc_apps.ovgo.R
 import nl.marc_apps.ovgo.domain.Departure
 import nl.marc_apps.ovgo.ui.preview.DayNightPreview
 import nl.marc_apps.ovgo.ui.preview.fixtures.DeparturePreviewParameterProvider
@@ -30,43 +25,20 @@ fun DeparturesList(
     imageLoader: ImageLoader? = null,
     onDepartureSelected: (Departure) -> Unit
 ) {
-    if (booleanResource(R.bool.is_large_screen_device)) {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            itemsIndexed(
-                departures,
-                key = { _, item -> item.journeyId },
-                contentType = { _, item -> item.isCancelled }
-            ) { index, departure ->
-                DepartureView(departure, imageLoader, onDepartureSelected)
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        itemsIndexed(
+            departures,
+            key = { _, item -> item.journeyId },
+            contentType = { _, item -> item.isCancelled }
+        ) { index, departure ->
+            DepartureView(departure, imageLoader, onDepartureSelected)
 
-                // TODO: Vertical divider
-
-                if (index != departures.lastIndex || index != departures.lastIndex - 1) {
-                    Divider(
-                        modifier = Modifier.padding(DepartureViewSpacingHorizontal, 0.dp)
-                    )
-                }
-            }
-        }
-    } else {
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            itemsIndexed(
-                departures,
-                key = { _, item -> item.journeyId },
-                contentType = { _, item -> item.isCancelled }
-            ) { index, departure ->
-                DepartureView(departure, imageLoader, onDepartureSelected)
-
-                if (index != departures.lastIndex) {
-                    Divider(
-                        modifier = Modifier.padding(DepartureViewSpacingHorizontal, 0.dp)
-                    )
-                }
+            if (index != departures.lastIndex) {
+                Divider(
+                    modifier = Modifier.padding(DepartureViewSpacingHorizontal, 0.dp)
+                )
             }
         }
     }
